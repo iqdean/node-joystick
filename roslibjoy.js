61761ee30d6de8e421fe0f24d922e0ce0bf80497
Ike Dean <iad1046@gmail.com>
2016-07-08 00:00:31 -0700

    final version, end-to-end tested with robot running rosbridge_server

--- roslibjoy.js
+++ roslibjoy.js
@@ -35,8 +35,8 @@ var ROSLIB = require('roslib');
 
 // following 3 variables get updated by joystick event handlers (aka callbacks)
 var safety = 'disabled';  
-var CV = 0;               // these 2 vars get sampled and published to ros as /cmd_vel message 
-var CAV = 0;              // on a periodic basis... lets use 10hz for now
+var CV = parseFloat(0);               // these 2 vars get sampled and published to ros as /cmd_vel message 
+var CAV = parseFloat(0);              // on a periodic basis... lets use 10hz for now
 
 var ros = new ROSLIB.Ros({
     //url : 'ws://localhost:9090'
@@ -62,72 +62,53 @@ angular : {
 }
 });
 
-function intToFloat(num, decPlaces) { return num.toFixed(decPlaces); }
+function intToFloat(num, decPlaces) { return parseFloat(num.toFixed(decPlaces)); }
 
 function publishToROS() {
+
+    // CV and CAV have to be a float otherwise u get a ROS type error on the other end
+
+    // [ERROR] [WallTime: 946686132.737454] [Client 3] [id: publish:/cmd_vel:16] publish: 
+    //         geometry_msgs/Twist message requires a float64 for field linear.x, but got a <type 'unicode'>
+
+    // SO, outside the ROSLIB.Message() function, use parseFloat(CV CAV) to set CV/CAV to a float value
 
     twist.linear.x = CV;
     twist.angular.z = CAV;
 
-    console.log("twist.linear.x  : ", twist.linear.x);
-    console.log("twist.angular.z : ", twist.angular.z);
+    //console.log("twist.linear.x  : ", twist.linear.x);
+    //console.log("twist.angular.z : ", twist.angular.z);
 
-    // cmdVel.publish(twist);
+    cmdVel.publish(twist);
 }
 
-setInterval(publishToROS, 1000);  // for debug, publish cmd_vel at rate of 1hz
+setInterval(publishToROS, 100);  // 1000ms = 1hz (debug), 100ms = 10hz (actual)
 
 joystick.on('button', function(event) {
 
-// { time: 17062012, value: 1, number: 13, type: 'button', id: 1 }
-// { time: 17062812, value: 0, number: 13, type: 'button', id: 1 }
-
     if (event.number === 13 && event.value === 1) {
 	safety = 'disabled';
-	console.log('safety : ', safety);
+	console.log('safety : ', safety, ' throttle engaged');
     }
 
     if (event.number === 13 && event.value === 0) {
 
 	safety = 'enabled';
-	CV = 0;  // Saftey enabled kill the throttle in case
-	CAV = 0; // it got released while the joystick wasn't near center
-	
-	// pubCMDvel() here
+	console.log('safety : ', safety, ' throttle disabled');
+	CV = parseFloat(0);   // Saftey enabled kill the throttle in case
+	CAV = parseFloat(0); // it got released while the joystick wasn't near center
 
-	//console.log('safety cv cav ', safety, CV, CAV);
     }
 
 });
 
 joystick.on('axis', function(event) {
 
-    //console.log(event);
-
-    // Scale Right Joystick X axis to a -1 < float_ry < +1 
-
-    //                         value  number     type
-    //{ time: 9078256, value:   xxx , number: 3, type: 'axis', id: 1 }
-    //                           |
-    //                -32767     0     +32767
-    //                 -1.00            +1.00
-    //               Full Left         Full Right
-
     if (safety === 'disabled' ) {
 
 	if (event.type === 'axis' && event.number === 3) {
 	    CAV = intToFloat(event.value/32768, 2);
-	    //console.log('CAV ', CAV);
 	}
-
-	// Scale Right Joystick Y axis to a -1 < float_ry < +1 
-
-	//                        -32767  +1 Full Fwd
-	//                           |
-	//                         value  number     type
-	//{ time: 9078256, value:   yyy , number: 3, type: 'axis', id: 1 }
-	//                           |
-	//                        +32767  -1 Full Rev
 
 	if (event.type === 'axis' && event.number === 4) {
 	    CV = intToFloat(event.value/-32768, 2);
