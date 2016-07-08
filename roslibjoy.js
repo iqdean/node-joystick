055373de0295bc760ec419c472e927bb1271d36c
Ike Dean <iad1046@gmail.com>
2016-07-07 20:40:58 -0700

    add roslib & console.log static twist msg

--- roslibjoy.js
+++ roslibjoy.js
@@ -1,40 +1,75 @@
+// 1 joystick.js
+//   $ npm install joystick
 // Set a deadzone of +/-3500 (out of +/-32k) and a sensitivty of 350 to reduce signal noise in joystick axis 
 
-// [IAD] REF: /home/iqdean/u1404/intel-edison/8-5-node-xbox-gamepad.txt
+// 2 [IAD] 
+// REF: /home/iqdean/u1404/intel-edison/8-5-node-xbox-gamepad.txt
 //            for xbox360 Game Controller button/joystick mappings
-
+//
 // Current Usage 
-
+//
 // Saftey Button - DPAD Up Button - HOLD PUSHED TO DRIVE
 // { time: 16346348, value: 0, number: 13, type: 'button', id: 1 }
 //                    |
 //                    |   safety    throttle
 //                    0 - enabled   disabled ( kills throttle control if u release the safety button)
 //                    1 - disabled  enabled  ( push and hold pushed to enable the throttle)
-
+//
 // Throttle - Right JoyStick
-  
+//  
 //                 +1 Full Fwd
 // 
 //   Full Lft -1    +   +1 Full Rt 
 //
 //                 -1 Full Rev
 
+// 3 [IAD] roslibjoy.js = 3-node-roslib-example/node-simple.js + 4-2-node-joystick/joystick.js
+//   Add roslib to joystick.js to publish /cmd_vel thru roslib to robot running rosbridge-server
+//
+//   $ npm install roslib   
+//     NOTE: For prereqs needed to get this working see intel-edison/8-5-node-xbox-gamepad.txt
 
-function intToFloat(num, decPlaces) { return num.toFixed(decPlaces); }
 
 var joystick = new (require('joystick'))(1, 3500, 350);
+var ROSLIB = require('roslib');
 
 // following 3 variables get updated by joystick event handlers (aka callbacks)
 var safety = 'disabled';  
 var CV = 0;               // these 2 vars get sampled and published to ros as /cmd_vel message 
 var CAV = 0;              // on a periodic basis... lets use 10hz for now
 
+var ros = new ROSLIB.Ros({
+    //url : 'ws://localhost:9090'
+    url : 'ws://192.168.2.15:9090'
+});
+
+var cmdVel = new ROSLIB.Topic({
+  ros : ros,
+  name : '/cmd_vel',
+  messageType : 'geometry_msgs/Twist'
+});
+
+var twist = new ROSLIB.Message({
+linear : {
+  x : 0.1,
+  y : 0.2,
+  z : 0.3
+},
+angular : {
+  x : -0.1,
+  y : -0.2,
+  z : -0.3
+}
+});
+
+function intToFloat(num, decPlaces) { return num.toFixed(decPlaces); }
+
 function publishToROS() {
 
     console.log('CV CAV : ', CV, CAV);
 
-    // TODO: add code to use roslib to format & publish /cmd_vel to ROS 
+    console.log("cmd_vel ", twist);
+    // cmdVel.publish(twist);
 }
 
 setInterval(publishToROS, 1000);  // for debug, publish cmd_vel at rate of 1hz
@@ -99,5 +134,15 @@ joystick.on('axis', function(event) {
 
 });
 
+ros.on('connection', function() {
+console.log('Connected to websocket server.');
+});
 
+ros.on('error', function(error) {
+console.log('Error connecting to websocket server: ', error);
+});
+
+ros.on('close', function() {
+console.log('Connection to websocket server closed.');
+});
 
